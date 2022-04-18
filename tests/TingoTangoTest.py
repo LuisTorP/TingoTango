6b22194b40eb7b52ddc9dde7b3a1915028bbb637
LuisTorP <72195998@continental.edu.pe>
2022-04-18 16:04:03 -0500

    Multiplo de 3 implementado V1.1

---
+++ tests/TingoTangoTest.py
@@ -0,0 +1,18 @@
+import unittest
+from src.logica.TingoTango import TingoTango
+
+class TingoTangoPrueba(unittest.TestCase):
+    def setUp(self):
+        self.TT=TingoTango()
+
+    def tearDown(self):
+        self.TT=None
+
+    def test_TingoTango_multiploTres_retornaTingo(self):
+        # Arrange
+            self.numero=6
+            self.resultadoEsperado = 'Tingo'
+        # Do
+            self.resultadoActual = self.TT.textoTingoTango (self.numero)
+        # Assert
+            self.assertEqual (self.resultadoEsperado, self.resultadoActual)
